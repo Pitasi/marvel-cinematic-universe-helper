26f934035d0037f192b763a4fdb58ec6ba7cdeb1
Antonio Pitasi <pitasi.antonio@gmail.com>
2016-05-15 15:39:00 +0200

    Lot of new stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,6 +6,22 @@ app.constant('$globals', {
   api_key: '032c03fbcaf94c05b55ddf4ec973ad16',
   endpoint: 'https://api.themoviedb.org/3/',
   strings: {
+    'home_title': {
+      'en': 'Homepage',
+      'it': 'Homepage'
+    },
+    'home_subtitle': {
+      'en': 'Heavily under construction',
+      'it': 'Ancora in stato embrionale'
+    },
+    'main_list': {
+      'en': 'Main movies list',
+      'it': 'Lista dei film principali'
+    },
+    'xmen_list': {
+      'en': 'X-Men movies list',
+      'it': 'Lista dei film sugli X-Men'
+    },
     'main_title': {
       'en': 'Suggested order',
       'it': 'Ordine consigliato'
@@ -20,7 +36,7 @@ app.constant('$globals', {
     },
     'footer_text': {
       'en': 'This website shows some useful informations to guide you through the huuuge list of movies made by Marvel Studios.',
-      'it': "Questo sito web mostra alcune informazioni utile per guidarti all'interno del gigantesco universo creato dai Marvel Studio"
+      'it': 'Questo sito web mostra alcune informazioni utile per guidarti all\'interno del gigantesco universo creato dai Marvel Studio.'
     }
   },
   languages: ['en', 'it']
@@ -30,8 +46,7 @@ app.constant('$globals', {
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'views/home.html',
-      controller: 'MainController'
+      redirectTo: '/en'
     })
     .when('/404', {
       //redirectTo: '/'
@@ -39,17 +54,19 @@ app.config(function ($routeProvider) {
     })
     .when('/:ln/', {
       templateUrl: 'views/home.html',
+      controller: 'HomeController'
+    })
+    .when('/list/:list', {
+      redirectTo: '/list/:list/en'
+    })
+    .when('/list/:list/:ln', {
+      templateUrl: 'views/movie_list.html',
       controller: 'MainController'
     })
-    .when('/movies.json', {
-      templateUrl: ' '
-
+    .when('/movie/:list/:id', {
+      redirectTo: '/movie/:list/:id/en'
     })
-    .when('/movie/:id', {
-      templateUrl: 'views/movie_details.html',
-      controller: 'MovieController'
-    })
-    .when('/movie/:id/:ln', {
+    .when('/movie/:list/:id/:ln', {
       templateUrl: 'views/movie_details.html',
       controller: 'MovieController'
     })
@@ -59,8 +76,32 @@ app.config(function ($routeProvider) {
 })
 
 // This controls home.html
+app.controller('HomeController', function($scope, $globals, $routeParams, $location) {
+  var languages = $globals.languages
+  var ln = $routeParams.ln
+  if (ln === undefined) ln = 'en'
+  if (languages.indexOf(ln) === -1) $location.path('/404')
+
+  $scope.ln = ln
+  $scope.home_title = $globals.strings.home_title[ln]
+  $scope.home_subtitle = $globals.strings.home_subtitle[ln]
+  $scope.main_list = $globals.strings.main_list[ln]
+  $scope.xmen_list = $globals.strings.xmen_list[ln]
+  $scope.footer_text = $globals.strings.footer_text[ln]
+
+  $scope.english_path = '#' + $location.path().replace('/' + ln, '/en')
+  $scope.italian_path = '#' + $location.path().replace('/' + ln, '/it')
+
+
+})
+// This controls movie_list.html
 app.controller('MainController', function ($scope, $globals, $routeParams, $location, $http) {
+$scope.go = function (p) { console.log(p); $location.path(p); }
+
   $scope.movies_list = []
+  var list = $routeParams.list
+  $scope.list = list
+  console.log(list)
   var languages = $globals.languages
   var ln = $routeParams.ln
   if (ln === undefined) ln = 'en'
@@ -69,8 +110,10 @@ app.controller('MainController', function ($scope, $globals, $routeParams, $loca
   $scope.main_title = $globals.strings.main_title[ln]
   $scope.read_more = $globals.strings.read_more[ln]
   $scope.footer_text = $globals.strings.footer_text[ln]
+  $scope.english_path = '#' + $location.path().replace('/' + ln, '/en')
+  $scope.italian_path = '#' + $location.path().replace('/' + ln, '/it')
 
-  $http.get('/res/movies_' + ln + '.json').success(function (data) {
+  $http.get('/res/' + list + '_' + ln + '.json').success(function (data) {
     var movies = data
     angular.forEach(movies, function (obj, key) {
       console.log('Loading movie: ' + key)
@@ -82,6 +125,7 @@ app.controller('MainController', function ($scope, $globals, $routeParams, $loca
 
 app.controller('MovieController', function ($scope, $globals, $routeParams, $location, $http) {
   var languages = $globals.languages
+  var list = $routeParams.list
   var id = $routeParams.id
   var ln = $routeParams.ln
   if (ln === undefined) ln = 'en'
@@ -89,8 +133,10 @@ app.controller('MovieController', function ($scope, $globals, $routeParams, $loc
   $scope.ln = ln
   $scope.footer_text = $globals.strings.footer_text[ln]
   $scope.imdb_text = $globals.strings.imdb_text[ln]
+  $scope.english_path = '#' + $location.path().replace('/' + ln, '/en')
+  $scope.italian_path = '#' + $location.path().replace('/' + ln, '/it')
 
-  $http.get('/res/movies_' + ln + '.json').success(function (data) {
+  $http.get('/res/' + list + '_' + ln + '.json').success(function (data) {
     var movies = data
     $scope.genres_list = []
     if (movies[id] !== undefined) {
@@ -98,6 +144,10 @@ app.controller('MovieController', function ($scope, $globals, $routeParams, $loc
       angular.forEach(obj.genres, function (k) {
         $scope.genres_list.push(k.name)
       })
+      if (obj.next !== undefined)
+        $scope.next = '#/movie/' + list + '/' + obj.next + '/' + ln
+        if (obj.previous !== undefined)
+          $scope.previous = '#/movie/' + list + '/' + obj.previous + '/' + ln
       $scope.title = obj.title
       $scope.poster = 'https://image.tmdb.org/t/p/w600/' + obj.poster_path
       $scope.poster_large = 'https://image.tmdb.org/t/p/w1000/' + obj.poster_path
--- download_movies.py
+++ download_movies.py
@@ -4,7 +4,7 @@ import json
 import requests
 
 api_key = '032c03fbcaf94c05b55ddf4ec973ad16'
-path = 'app/res/movies_{}.json'
+path = 'app/res/{}_{}.json'
 
 languages = ['it', 'en']
 movies = [
@@ -22,10 +22,22 @@ movies = [
     '102899', # Ant-Man
     '271110' # Captain America: Civil War
 ]
+xmen_movies = [
+    '36657', # X-Men
+    '36658', # X-Men 2
+    '36668', # X-Men - Conflitto finale
+    '2080', # X-Men le origini - Wolverine
+    '49538', # X-Men - L'inizio
+    '76170', # Wolverine - L'immortale
+    '127585', # X-Men - Giorni di un futuro passato
+    '293660', # Deadpool
+    '246655' # X-Men - Apocalisse
+]
 
 res = {}
 base_url = 'https://api.themoviedb.org/3/movie/{}?api_key={}&language={}'
 
+# Download main movies
 for l in languages:
     for i in range(0, len(movies)):
         r = requests.request('get', base_url.format(movies[i], api_key, l))
@@ -33,12 +45,48 @@ for l in languages:
             while True:
                 response = json.loads(r.text)
                 response['order'] = i+1
+
+                if i+1 in range(0, len(movies)):
+                    response['next'] = movies[i+1]
+
+                if i-1 in range(0, len(movies)):
+                    response['previous'] = movies[i-1]
+
                 res[movies[i]] = response
                 try:
                     if response['status_code'] == 25:
+                        print('Limit reached. Sleep for a while...')
                         sleep(1)
                 except Exception:
                     print("Download complete: " + movies[i])
                     break
-    with open(path.format(l), 'w') as f:
+    with open(path.format('main', l), 'w') as f:
+        json.dump(res, f)
+
+
+res = {}
+# Download X-Men movies
+for l in languages:
+    for i in range(0, len(xmen_movies)):
+        r = requests.request('get', base_url.format(xmen_movies[i], api_key, l))
+        if r.ok:
+            while True:
+                response = json.loads(r.text)
+                response['order'] = i+1
+
+                if i+1 in range(0, len(xmen_movies)):
+                    response['next'] = xmen_movies[i+1]
+
+                if i-1 in range(0, len(xmen_movies)):
+                    response['previous'] = xmen_movies[i-1]
+
+                res[xmen_movies[i]] = response
+                try:
+                    if response['status_code'] == 25:
+                        print('Limit reached. Sleep for a while...')
+                        sleep(1)
+                except Exception:
+                    print("Download complete: " + xmen_movies[i])
+                    break
+    with open(path.format('xmen', l), 'w') as f:
         json.dump(res, f)
